3342eb2e0bcf629920947f434b91eaa81119619a
Sergio <sergiiy@tallium.com>
2019-10-06 01:38:26 +0300

    Developing form

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,15 +3,35 @@ import React, {Component} from 'react';
 import './item-add-form.css'
 
 export default class ItemAddForm extends Component {
+
+    state = {
+        label: '',
+    };
+
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        })
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.addItem(this.state.label);
+        this.setState({
+            label: ''
+        })
+    };
+
     render(){
         return (
-            <div className="item-add-form">
-                <button className="btn btn-outline-secondary mt-3"
-                    onClick = {() => this.props.addItem('Hello')}
-                >
+            <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+                <input type="text" className="form-control" onChange={this.onLabelChange}
+                       placeholder="What needs to be done" value={this.state.label}
+                />
+                <button className="btn btn-outline-secondary" style={{width: '40%'}}>
                     Add Item
                 </button>
-            </div>
+            </form>
         )
     }
 }
